a62e85f480995bbf1ec18727522ec4a011310e6d
alligatormonday <joey@joeyjepson.com>
2020-09-22 19:11:48 -0700

    Created ajax call with api key and hash.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+    console.log(md5("want"))
+
+    let publicKey = "b96debf8a06e6f68f22924e2c930b450"
+    let privateKey = "274ea5c7e32c0faf4d49d5ffc01a19459d78299a"
+    let ts = 1;
+    let hash = md5(ts+privateKey+publicKey);
+    
+    $.ajax({
+        url:"https://gateway.marvel.com/v1/public/characters?apikey=b96debf8a06e6f68f22924e2c930b450&ts=1&hash=" + hash,
+        method: "GET"
+    }).then(function (response){
+        console.log(response)
+    })
 
 
 
